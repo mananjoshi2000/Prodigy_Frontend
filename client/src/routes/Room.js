804de3e33a0bc401fd2f98c63499dd56c2fe4694
Neel Makadiya <NeelMakadiya14@github.com>
2020-11-06 12:11:19 +0530

    Make it Better

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -104,6 +104,20 @@ const Room = (props) => {
         const peer = new Peer({
             initiator: true,
             trickle: false,
+            config: {
+                iceServers: [
+                    {
+                        urls: "stun:numb.viagenie.ca",
+                        username: "sultan1640@gmail.com",
+                        credential: "98376683"
+                    },
+                    {
+                        urls: "turn:numb.viagenie.ca",
+                        username: "sultan1640@gmail.com",
+                        credential: "98376683"
+                    }
+                ]
+            },
             stream,
         });
 
@@ -118,6 +132,20 @@ const Room = (props) => {
         const peer = new Peer({
             initiator: false,
             trickle: false,
+            config: {
+                iceServers: [
+                    {
+                        urls: "stun:numb.viagenie.ca",
+                        username: "sultan1640@gmail.com",
+                        credential: "98376683"
+                    },
+                    {
+                        urls: "turn:numb.viagenie.ca",
+                        username: "sultan1640@gmail.com",
+                        credential: "98376683"
+                    }
+                ]
+            },
             stream,
         })
 
