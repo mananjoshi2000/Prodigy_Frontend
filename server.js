64e8303c58ce3f015f38ae9ce9e6e3bf26597098
Neel Makadiya <NeelMakadiya14@github.com>
2020-11-06 09:53:40 +0530

    Make it Better

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const path=require('path');
 const express = require("express");
 const http = require("http");
@@ -55,13 +56,11 @@ io.on('connection', socket => {
 });
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+if(process.env.PROD){
+    app.use(express.static(path.join(__dirname,'./client/build')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'./client/build/index.html'));
+    })
 }
 
 const port=process.env.PORT || 8000;
